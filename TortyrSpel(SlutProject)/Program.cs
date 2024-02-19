b65b7c9c3c46251c66808aad1d8a5ff3f5172e14
Axel Grufman <Axel.Grufman@elev.ga.ntig.se>
2024-02-19 09:31:29 +0100

    First lesson

--- TortyrSpel(SlutProject)/Program.cs
+++ TortyrSpel(SlutProject)/Program.cs
@@ -1,4 +1,14 @@
-﻿
+﻿String Level = "Start";
+int[,] board = new int[100, 30];
+for (int y = 0; y < board.GetLength(1); y++)
+{
+  for (int x = 0; x < board.GetLength(0); x++)
+  {
+    Console.Write(board[x, y]); // Ritar ut den nuvarande rutan
+  }
+  Console.WriteLine(); // Lägger till en radbrytning i slutet på varje utritad rad.
+}
+
 
 
 Console.ReadLine();
